be9a248602cd024aaf12a3ca1059261a401d9517
Derek Kaye <github@dezzanet.co.uk>
2014-11-21 20:49:15 +0000

    Adjust iplayer_renamer.pl to use new tv-management script and config files

--- iplayer_renamer.pl
+++ iplayer_renamer.pl
@@ -1,93 +1,124 @@
 #!/usr/bin/perl
 use strict;
-my $dir = "/media/media/media/media/iplayer";
-my $tv_base = "/media/media/media/media/TV";
-my $unwatched = "/media/media/media/media/unwatched";
-opendir(D, $dir) || die "Can't opedir: $!\n";
-while (my $f = readdir(D)) {
-    if ($f =~ /default(\.flv|\.mp4)$/) {
-        my $extension = $1;
-        my $programme;
-        my $series = -1;
-        my $episode = -1;
-        my $title = -1;
-        my $outfilename;
-        my $out_dir;
-        my $sym_dir;
-        my $command;
-        my @parts = split(/\_\-\_/, $f, 2);
-        if ($parts[0] =~ /\_Series\_[0-9]+$/) {
-            # We have a series. Get the programme and series number by splitting around _Series_
-            ($programme, $series) = split(/\_Series\_/, $parts[0], 2);
-            $programme =~ s/\_/ /g;
-        } else {
-            $programme = $parts[0];
-            $programme =~ s/\_/ /g;
+use warnings;
+use Getopt::Long;
+use Carp;
+use English qw(-no_match_vars);
+use Config::Tiny;
+use Cwd;
+
+my $dir_sep = q{/};
+
+# Initialise command line options to default
+my $verbose     = 0;
+my $config_file = undef;
+
+GetOptions(
+    'verbose'         => \$verbose,
+    'configuration=s' => \$config_file,
+);
+
+my $config = get_configuration($config_file);
+
+my $tv_manager  = $config->{_}->{tv_manager_path};
+my $iplayer_dir = $config->{_}->{iplayer_dir};
+
+# Check config
+if ( ! $tv_manager || ! $iplayer_dir) {
+    croak('Invalid configuration');
+}
+
+my $result = opendir D, $iplayer_dir;
+if ( ! $result) {
+    croak('Failed to open iplayer directory: ' . $OS_ERROR);
+}
+while (my $file = readdir D) {
+    if ($file =~ /default(\.flv|\.mp4)$/xms) {
+        verbose('Found file ' . $file);
+        my $command = $tv_manager
+                    . ' --move-file'
+                    . ' --add-to-unwatched';
+        if (defined $config_file) {
+            $command .= ' --configuration ' . $config_file;
         }
-        # Chop the dull stuff off the end
-        my $endpart = $parts[1];
-        $endpart =~ s/\_[a-z0-9]{8}\_default$extension//;
-        if ($endpart =~ /^Episode\_([0-9]+)(.*)/) {
-            # We have an Episode x format
-            $episode = $1;
-            $episode =~ s/^Episode\_//;
-            if ($2) {
-                $title = $2;
-            }
-        } elsif ($endpart =~ /^([0-9]+)\.(.*)/) {
-            # We have a x. format
-            $episode = $1;
-            if ($2) {
-                $title = $2;
-            }
-        } else {
-            # No episode
-            $title = $endpart;
-        }
-        $outfilename = $programme;
-        if (!-d "$tv_base/$programme") {
-            # Make the directory
-            if (!mkdir "$tv_base/$programme") {
-                print "Couldn't create directory $tv_base/$programme";
-                next;
-            }
-        }
-        $out_dir = "$tv_base/$programme";
-        if ($series != -1) {
-            $outfilename .= " " . $series . "x";
-            if (!-d "$tv_base/$programme/$programme $series" . "x") {
-                # Make the directory
-                if (!mkdir "$tv_base/$programme/$programme $series" . "x") {
-                    print "Couldn't create directory $tv_base/$programme/$programme $series" . "x";
-                    next;
-                }
-            }
-            $out_dir = "$tv_base/$programme/$programme $series" . "x";
-        } else {
-            $outfilename .= " ";
-        }
-        if ($episode != -1) {
-            if ($episode <10) {
-                $episode = '0' . $episode;
-            }
-            $outfilename .= $episode;
-        }
-        if ($title != -1) {
-            $title =~ s/\_/ /g;
-            $title =~ s/^\ //g;
-            $outfilename .= " - " . $title;
-        }
-        $outfilename .= $extension;
-        $outfilename =~ s/  / /g;
-        if (rename($dir . "/$f", $out_dir . "/$outfilename")) {
-            print "Moved $f to $outfilename\n";
-            $sym_dir = $out_dir;
-            $sym_dir =~ s/^$tv_base/\.\.\/TV/;
-            $command = "ln -s \"$sym_dir/$outfilename\" \"$unwatched/$outfilename\"";
-            system($command);
-        } else {
-            print "Failed moving $dir/$f to $out_dir/$outfilename\n";
-        }
+        $command .= q{ } . $iplayer_dir . $dir_sep . $file;
+        verbose('Running following command: ' . $command);
+        system $command;
     }
 }
 closedir(D);
+
+sub usage {
+    my $usage = << '_END_USAGE_';
+Usage:
+iplayer_rename
+
+Scans for files in the iplayer folder that have finished downloading and moves
+them to the right place then adds them to the unwatched list
+
+Additional options:
+
+  --verbose
+    Display verbose debugging output
+
+  --configuration filename
+    Use the specified configuration. If omitted, will look for a file called
+    .tv-management.ini in the current directory or the user's home folder
+
+Configuration:
+
+  A simple ini file with the following settings
+
+  tv_manager_path
+    Path to the tv-management executable
+
+  iplayer_dir
+    Directory to scan for iplayer content
+
+_END_USAGE_
+
+    print $usage;
+    return;
+}
+
+# @todo - not sure if the below can be moved to a common location before we
+# know paths. Perhaps we need a package for this stuff
+sub verbose {
+    my ($message) = @_;
+    if ($verbose) {
+        print $message . "\n";
+    }
+    return;
+}
+
+sub get_configuration {
+    my ($conf_file) = @_;
+    if ( ! defined $conf_file) {
+        $conf_file = get_default_config_file();
+        if ( ! defined $conf_file) {
+            my $message = 'Unable to find a config file to use';
+            usage();
+            croak($message);
+        }
+    }
+    verbose('Using config file: ' . $conf_file);
+
+    return Config::Tiny->read($conf_file);
+}
+
+sub get_default_config_file {
+    verbose('Determining default config file');
+    my $default_filename = '.tv-management.ini';
+    # We default to current_dir/.tv-management.ini
+    # Then ~/.tv-management.ini
+    my $dir = getcwd;
+    my @files_to_test;
+    push @files_to_test, $dir . $dir_sep . $default_filename;
+    push @files_to_test, $ENV{HOME} . $dir_sep . $default_filename;
+    for my $filename (@files_to_test) {
+        if ( -e $filename) {
+            return $filename;
+        }
+    }
+    return;
+}
